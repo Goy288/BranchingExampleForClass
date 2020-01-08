b384a10cb60416d5a6970aebebe9f264113bf1b0
Cameron Sherwood <cameronsherwood1@gmail.com>
2020-01-08 14:25:08 -0800

    Created a student database script.

---
+++ BranchingExampleForClass/DBScripts/CreateStudentDB.sql
@@ -0,0 +1,14 @@
+﻿CREATE DATABASE Student
+GO
+
+USE Student
+GO
+
+CREATE TABLE Student
+(   --NAME--		 --TYPE--       --NULL?--
+	FirstName		varchar(32)		NOT NULL
+,	LastName		varchar(32)		NOT NULL
+,	DateOfBirth		date			NOT NULL
+,	CellPhone		char(10)		NULL
+,	IsImmortal		bit				NOT NULL
+)
